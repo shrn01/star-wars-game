0bdc36155f959d88a76d05e956249687a898ff1c
Sharan Js <sharanjs1999@gmail.com>
2020-06-08 21:13:38 +0530

    Added background

--- StarWars/Star-wars.py
+++ StarWars/Star-wars.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 from random import randint
 from pygame import mixer
 
@@ -25,10 +26,11 @@ def show_score():
 	screen.blit(scoreText,(textX,textY))
 
 icon = pygame.image.load("icon.png")
+bg = pygame.image.load('bg.jpg')
 pygame.display.set_icon(icon)
 falcon = pygame.image.load("falcons.png")
 xshot = pygame.image.load("xshot45.png")
-sd = pygame.image.load("sd100.png")
+sd = pygame.image.load("sd120.png")
 bullet = pygame.image.load("bbullet.png")
 
 
@@ -40,7 +42,7 @@ enemyY = []
 
 
 def new_xshot():
-	return randint(0,wt-46),randint(30,ht-105)
+	return randint(0,wt-46),randint(30,ht-300)
 
 
 for i in range(10):
@@ -51,11 +53,11 @@ for i in range(10):
 dx = 0
 dy = 0
 
-ex = 0.1
-ey = 0 
+ex = list(randint(5,30)/100 * random.choice([-1,1]) for i in range(10))
+ey = 0.1
 
-bY = []
-bX = []
+bulletY = []
+bulletX = []
 shoot = False
 
 ty = -130
@@ -66,10 +68,11 @@ def dist(i,j,k,l):
 def draw_ship(x,y,z):
 	screen.blit(z,(int(x),int(y)))
 
-ex = list(randint(5,30)/100 for i in range(10))
+
 running =True
 while running:
 	screen.fill((20,20,20))
+	screen.blit(bg,(0,0))
 
 
 
@@ -79,19 +82,19 @@ while running:
 
 
 		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE:
+				bulletX.append(playerX + 25)
+				bulletY.append(ht - 100)
 			if event.key == pygame.K_LEFT:
 				dx = -0.5
 			if event.key == pygame.K_RIGHT:
 				dx = 0.5
-			if event.key == pygame.K_SPACE:
-				bX.append(playerX + 25)
-				bY.append(ht - 100)
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 				dx = 0
 	if ty <3:
 		ty += 0.04
-	draw_ship(200,ty,sd)
+	draw_ship(190,ty,sd)
 	
 	if playerX>wt-64:
 		playerX = wt-64
@@ -102,34 +105,35 @@ while running:
 	
 
 	for i in range(10):
-		enemyX[i] += ex[i]
+		enemyX[i] = enemyX[i] + ex[i]
+		enemyY[i] += ey
 		draw_ship(enemyX[i],enemyY[i],xshot)
 		if enemyX[i] < 0:
-			ex[i] = -ex[i]
+			ex[i] = -1 * ex[i]
 		if enemyX[i] > wt-46:
-			ex[i] = -ex[i]
+			ex[i] = -1 * ex[i]
 
 
-	for i in range(len(bX)):
-		bY[i] -= 0.4
-		draw_ship(bX[i],bY[i],bullet)
+	for i in range(len(bulletX)):
+		bulletY[i] -= 0.4
+		draw_ship(bulletX[i],bulletY[i],bullet)
 		# bullet_sound = mixer.Sound("bullet.mp3 ")
 		# bullet_sound.play()
 
-	no = len(bX)
+	no = len(bulletX)
 	for i in range(no):	
 		try:
-			if bY[i]<0:
-				del bY[i]
-				del bX[i]
+			if bulletY[i]<0:
+				del bulletY[i]
+				del bulletX[i]
 				no -= 1
 			for j in range(10):
-				if dist(bX[i]+2,bY[i]+2,enemyX[j]+32,enemyY[j]+32) < 500:
-					enemyX[j],enemyY[j] = randint(0,wt-46),randint(30,ht-105)
+				if dist(bulletX[i]+2,bulletY[i]+2,enemyX[j]+32,enemyY[j]+32) < 500:
+					enemyX[j],enemyY[j] = randint(0,wt-46),randint(30,ht-300)
 					ex[i] = randint(5,30)/100
 					score += 1
-					del bX[i]
-					del bY[i]
+					del bulletX[i]
+					del bulletY[i]
 
 		except:
 			pass
